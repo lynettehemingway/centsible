b179a51629aadaea6575a1fc79a5c96234db657d
CloudRazerz <cloudrazerz@gmail.com>
2025-03-25 14:24:02 -0400

    added signup route and mongodb integration

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,4 +1,7 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env.local" });
 
 const uri = process.env.ATLAS_URI || "";
 const client = new MongoClient(uri);
@@ -7,12 +10,12 @@ try {
     await client.connect();
 
     await client.db("test").command({ping: 1});
-    console.log("Ping successful");
+    console.log("Ping Successful");
 
 } catch(err){
     console.error(err);
 }
 
-let db = client.db("users");
+let db = client.db("centsible");
 
 export default db;
--- server/routes/users.js
+++ server/routes/users.js
@@ -5,43 +5,35 @@ import db from "../db/connection.js";
 
 const router = express.Router();
 
-
-//testing, replace with db
-const test_users = [
-  {
-  "name": "Alice", "password": "123"
-  }
-  ]
-
-
 router.post('/login', async (req, res) => {
-  const user = test_users.find(user => user.name === req.body.name);
-  if (!user) return res.status(400).send('User not found');
-
-
-  //to do: implement database so we can actually use encrypted passwords
+  let collection = await db.collection("users");
+  let user = await collection.findOne({name: req.body.name});
+  if (!user) return res.status(401).send('Invalid Credentials');
 
   try {
-    //if (await bcrypt.compare(req.body.password, user.password)) {
-    if (req.body.password == user.password){
-      res.send('Login successful');
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.status(200).send('Login Successful');
     } else {
-      res.status(401).send('Wrong password');
+      res.status(401).send('Invalid Credentials');
     }
   } catch {
     res.status(500).send();
   }
 });
 
-router.get('/', (req, res) => res.json(test_users));
+//router.get('/', (req, res) => res.json(test_users));
 
 router.post('/', async (req, res) => {
+  let collection = await db.collection("users");
+  let result = await collection.findOne({email: req.body.email});
+  if (result) return res.status(400).send('Account Already Exists')
+
   try {
     const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
     const user = { name: req.body.name, email: req.body.email, password: hashedPassword };
-    test_users.push(user);
-    res.status(201).send();
+    collection.insertOne(user);
+    res.status(201).send('Account Created');
   } catch {
     res.status(500).send();
   }
