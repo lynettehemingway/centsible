e73044242448730bb88de250d6c8324395715a4a
Alice Jiang <zhouzhoujiang12@gmail.com>
2025-03-24 23:05:07 -0400

    react router to check if user auth

--- Centsible/app/index.js
+++ Centsible/app/index.js
@@ -1,7 +1,21 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
+import {Link, useRouter} from 'expo-router'
+import service from './../utils/services'
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(()=> {
+    checkUserAuth();
+  }, [])
+
+  const checkUserAuth = async() => {
+    const result = await services.getData('login');
+    if(result != 'true') {
+      router.replace('/login')
+    }
+    console.log("Result", result);
+  }
   return (
     <View>
       <Text>Home</Text>
